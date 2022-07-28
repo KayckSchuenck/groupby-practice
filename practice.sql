744ec4803937e46e98f1da602f2f50bef75951a9
KayckSchuenck <ycferreiras@gmail.com>
2022-07-28 18:03:27 -0300

    3 questoes prontas

--- practice.sql
+++ practice.sql
@@ -1,8 +1,15 @@
-1_
-SELECT users.id,users.name AS name, cities.name AS city from users JOIN cities ON users."cityId"=cities.id;
+1-
+SELECT COUNT(experiences."endDate") as "currentExperiences" From experiences;
 
-2_
-SELECT testimonials.id, user1.name AS writer, user2.name AS recipient, testimonials.message
-FROM testimonials
-JOIN  users user1 ON testimonials."writerId" = user1.id
-JOIN  users user2 ON testimonials."recipientId" = user2.id
+2-
+SELECT COUNT(courses.id) AS educations, users.id 
+FROM educations 
+JOIN users ON users.id=educations."userId"
+JOIN courses ON educations."courseId"= courses.id 
+GROUP BY users.id
+
+3-
+SELECT COUNT(testimonials.id) AS "testimonialCount", users.name AS writer 
+FROM testimonials 
+JOIN users ON testimonials."writerId"=users.id
+WHERE testimonials."writerId"=435
